fc143434bb672b2e89a7752626346ecb46677d74
zZuccoli <132960683+zZuccoli@users.noreply.github.com>
2024-10-07 18:57:57 +0000

    add scripts  `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto prático para treino de programação... Implementando : https://www.tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-function Home(){
-    return <h1>Lívia...Se voce me acha um bom namorado me fala 🥩BIFE ACEBOLADO🧅  </h1>
+function Home() {
+  return (
+    <h1>Lívia...Se voce me acha um bom namorado me fala 🥩BIFE ACEBOLADO🧅 </h1>
+  );
+}
+
+function test() {
+  console.log("teste");
 }
 
 export default Home;
